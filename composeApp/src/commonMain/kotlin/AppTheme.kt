a628b644035bb4b1708329f0b217b3add62e434a
Matias-sh <devmatiasbritez@gmail.com>
2024-08-26 16:42:43 -0300

    Estructuracion

---
+++ composeApp/src/commonMain/kotlin/AppTheme.kt
@@ -0,0 +1,28 @@
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+
+}
+
+@Composable
+fun getColorsTheme() {
+    val isDarkMode = false
+
+    
+}
